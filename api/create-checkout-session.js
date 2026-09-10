bfd06a05e5733cb83d8592977cf13e4662889abc
sbhsxmxxxxx-collab <sbhsxmxxxxx@gmail.com>
2026-09-10 07:52:58 +0700

    Update create-checkout-session.js

--- api/create-checkout-session.js
+++ api/create-checkout-session.js
@@ -81,7 +81,7 @@ const PRODUCTS = {
   76: { name: 'MacBook Neo · 512 Go',              price: 559  },
   77: { name: 'MacBook Pro M5 · 1 To',             price: 1429 },
   78: { name: 'MacBook Air 13" (2026) · 512 Go',   price: 909  },
-  79: { name: 'AirPods Pro 2',                     price: 139  },
+  79: { name: 'AirPods Pro 2',                     price: 49   },
   80: { name: 'AirPods Max',                       price: 249  },
   81: { name: 'Apple Watch Ultra 2',                price: 519  },
   82: { name: 'Apple Watch Ultra 3',                price: 589  },
@@ -146,6 +146,7 @@ module.exports = async (req, res) => {
         allowed_countries: ['AC','AD','AE','AF','AG','AI','AL','AM','AO','AQ','AR','AT','AU','AW','AX','AZ','BA','BB','BD','BE','BF','BG','BH','BI','BJ','BL','BM','BN','BO','BQ','BR','BS','BT','BV','BW','BY','BZ','CA','CD','CF','CG','CH','CI','CK','CL','CM','CN','CO','CR','CV','CW','CY','CZ','DE','DJ','DK','DM','DO','DZ','EC','EE','EG','EH','ER','ES','ET','FI','FJ','FK','FO','FR','GA','GB','GD','GE','GF','GG','GH','GI','GL','GM','GN','GP','GQ','GR','GS','GT','GU','GW','GY','HK','HN','HR','HT','HU','ID','IE','IL','IM','IN','IO','IQ','IS','IT','JE','JM','JO','JP','KE','KG','KH','KI','KM','KN','KR','KW','KY','KZ','LA','LB','LC','LI','LK','LR','LS','LT','LU','LV','LY','MA','MC','MD','ME','MF','MG','MK','ML','MM','MN','MO','MQ','MR','MS','MT','MU','MV','MW','MX','MY','MZ','NA','NC','NE','NG','NI','NL','NO','NP','NR','NU','NZ','OM','PA','PE','PF','PG','PH','PK','PL','PM','PN','PR','PS','PT','PY','QA','RE','RO','RS','RU','RW','SA','SB','SC','SD','SE','SG','SH','SI','SJ','SK','SL','SM','SN','SO','SR','SS','ST','SV','SX','SZ','TA','TC','TD','TF','TG','TH','TJ','TK','TL','TM','TN','TO','TR','TT','TV','TW','TZ','UA','UG','US','UY','UZ','VA','VC','VE','VG','VN','VU','WF','WS','XK','YE','YT','ZA','ZM','ZW','ZZ'],
       },
       phone_number_collection: { enabled: true },
+      allow_promotion_codes: true,
       success_url: `${origin}/success.html?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${origin}/index.html`,
     });
